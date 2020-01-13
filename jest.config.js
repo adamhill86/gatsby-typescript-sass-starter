fac9b9bab753d739a7b2ddfab27d7c8d27fe28bc
Adam Hill <ahill@costar.com>
2020-01-13 10:00:39 -0500

    Minor lint fix

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/*.spec.ts', '**/*.spec.tsx'],
   transform: {
-      '^.+\\.(ts|tsx|html)$': 'ts-jest',
+    '^.+\\.(ts|tsx|html)$': 'ts-jest',
   },
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": 'identity-obj-proxy',
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": '<rootDir>/__mocks__/file-mock.js',
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
   },
   testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
@@ -15,5 +15,5 @@ module.exports = {
     __PATH_PREFIX__: '',
   },
   setupFiles: ['<rootDir>/loadershim.js'],
-  setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
+  setupFilesAfterEnv: ['<rootDir>/setup-test-env.js'],
 };
